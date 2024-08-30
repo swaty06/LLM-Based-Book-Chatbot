0d53ef6d65547398c997ccc8d5b13b045eade370
swaty06 <sujswa@gmail.com>
2024-08-30 14:30:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import streamlit as st
 import importlib
+import os
+
+from PIL import Image
+from streamlit_option_menu import option_menu
+from utils import apply_background,custom_navbar
+from st_pages import Page, show_pages, add_page_title
 from pathlib import Path
 
 root_path = Path(__file__).parent
+media_path = root_path.joinpath("media")
 pages_path = root_path.joinpath('Pages')
 
 # Set page configuration
@@ -24,7 +31,7 @@ def load_page(page_name):
         module = importlib.import_module(module_name)
         return module
     except ModuleNotFoundError:
-        st.error(f"{page_name} not found.")
+        st.error(f" {page_name} not found.")
         return None
 
 # Display the selected page with content
@@ -36,13 +43,12 @@ else:
     if page_module:
         page_module.app()  # Call the app function from the module
 
-# Main content in the sidebar
-st.sidebar.subheader("Tools Available")
-st.sidebar.write("""
+# Main content (previously in the sidebar)
+st.subheader("Tools Available")
+st.write("""
 1. **URL Summarization:** Provide a URL related to any book or literary content, and our tool will summarize the key points for you.
 2. **Book Chatbot:** Ask any book-related questions, and get insightful responses from our AI-powered assistant.
 """)
-
 # Add links to book resources or related pages
 st.markdown("""
 For more information and about the project, check out my profile:
@@ -58,3 +64,14 @@ st.markdown(
     **About:** This application is designed to help you explore books, get recommendations, and find your next favorite read. Stay tuned for more updates!
     """
 )
+
+show_pages(
+    [
+        Page(str(root_path.joinpath("main.py")), "Home", "🏠"),
+        Page(str(pages_path.joinpath("page1.py")), "Url Summarisor", ":books:"),
+        Page(str(pages_path.joinpath("page2.py")), "BookBot", "🌿"),
+       
+    ]
+)
+
+add_page_title(layout="wide")
