f41214f76be5ba81140b3ce4b6716f4e6597ee1e
swaty06 <sujswa@gmail.com>
2024-09-13 22:47:10 +0200

    Update langchain_helper.py

--- langchain_helper.py
+++ langchain_helper.py
@@ -1,17 +1,8 @@
-
-
 from langchain.vectorstores import FAISS
-from langchain_community.llms.google_palm import GooglePalm
-#from google.generativeai import GooglePaLMClient
-#from langchain_community.llms.google_gemini import GoogleGemini
-from langchain_google_genai import GoogleGenerativeAI
+from langchain.llms import GooglePalm
 from langchain.document_loaders.csv_loader import CSVLoader
 from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.prompts import PromptTemplate
-import google.generativeai as palm
-from langchain.llms.base import LLM
-from typing import List, Optional, Dict, Any
-
 from langchain.chains import RetrievalQA
 import os
 
@@ -19,40 +10,14 @@ from dotenv import load_dotenv
 load_dotenv()  # take environment variables from .env (especially openai api key)
 
 # Create Google Palm LLM model
-#llm = GooglePalm(google_api_key=os.environ["GOOGLE_API_KEY"], temperature=0.1)
-#llm =GoogleGemini(api_key=os.environ["GOOGLE_API_KEY"], temperature=0.1)
-#llm = GoogleGenerativeAI(google_api_key=os.environ["GOOGLE_API_KEY"], temperature=0.1)
-llm =palm.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-
-class GooglePaLM_LLM(LLM):
-    def _call(self, prompt: str, stop: Optional[List[str]] = None) -> str:
-        """Make a call to Google PaLM API and return the output."""
-        response = palm.generate_text(
-            model="models/text-bison-001",  # Specify the model you're using
-            prompt=prompt,
-            temperature=0.7  # Configure this as needed
-        )
-        return response.result
-
-    @property
-    def _llm_type(self) -> str:
-        """Return the type of the LLM."""
-        return "google_palm"
-
-    @property
-    def _identifying_params(self) -> Dict[str, Any]:
-        """Return the identifying parameters of the LLM."""
-        return {"model": "models/text-bison-001"}
-
-
-
+llm = GooglePalm(google_api_key=os.environ["GOOGLE_API_KEY"], temperature=0.1)
 # # Initialize instructor embeddings using the Hugging Face model
 instructor_embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-large")
 vectordb_file_path = "faiss_index"
 
 def create_vector_db():
     # Load data from FAQ sheet
-    loader = CSVLoader(file_path='faq.csv', source_column="prompt")
+    loader = CSVLoader(file_path='codebasics_faqs.csv', source_column="prompt")
     data = loader.load()
 
     # Create a FAISS instance for vector database from 'data'
@@ -65,7 +30,7 @@ def create_vector_db():
 
 def get_qa_chain():
     # Load the vector database from the local folder
-    vectordb = FAISS.load_local(vectordb_file_path, instructor_embeddings,allow_dangerous_deserialization=True)
+    vectordb = FAISS.load_local(vectordb_file_path, instructor_embeddings)
 
     # Create a retriever for querying the vector database
     retriever = vectordb.as_retriever(score_threshold=0.7)
@@ -79,10 +44,8 @@ def get_qa_chain():
     QUESTION: {question}"""
 
     PROMPT = PromptTemplate(
-        template=prompt_template,
-        input_variables=["context", "question"]
+        template=prompt_template, input_variables=["context", "question"]
     )
-    llm = GooglePaLM_LLM()
 
     chain = RetrievalQA.from_chain_type(llm=llm,
                                         chain_type="stuff",
@@ -96,4 +59,3 @@ def get_qa_chain():
 if __name__ == "__main__":
     create_vector_db()
     chain = get_qa_chain()
-    
