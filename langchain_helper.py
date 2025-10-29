703e420b65bd19248379e011352eeee88bc3e81d
SWATHY RAMAKRISHNAN <sujswa@gmail.com>
2025-10-29 12:20:41 +0100

    Update langchain_helper.py

--- langchain_helper.py
+++ langchain_helper.py
@@ -18,7 +18,7 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 # Step 2: Define a custom Gemini wrapper for LangChain
 class GeminiLLM(LLM):
-    #def __init__(self, model_name="gemini-1.5-flash"):
+    #def __init__(self, model_name="gemini-2.5-flash"):
         #self.model_name = model_name
 
     def _call(self, prompt: str, stop: Optional[List[str]] = None) -> str:
@@ -89,3 +89,4 @@ def get_qa_chain():
 if __name__ == "__main__":
     create_vector_db()
     chain = get_qa_chain()
+
